408f39ec611eefb0db4c4d775c943c8915124cc9
Caleb Ng <caleb17ng@gmail.com>
2021-08-24 01:37:46 -0700

    changed GET request handling

--- server/index.js
+++ server/index.js
@@ -14,9 +14,13 @@ app.get("/api", (req, res)=> {
 });
 
 //all other get requests not handled before will return our react app
-app.get("*", (req, res)=>{
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-})
+// app.get("*", (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'../client/build/index.html'));
+  });
 
 app.listen(PORT, ()=> {
     console.log(`Server listening on ${PORT}`);
